4340cbfed554b1423c4ef2c8937b45cbd4961608
George Lewis <schvin@schvin.net>
2014-06-08 03:58:45 +0000

    some basic testing for #5

--- scripts/ingest.pl
+++ scripts/ingest.pl
@@ -1,26 +1,42 @@
 #!/usr/bin/perl
 
+use Carp;
+use strict;
+use warnings;
+
 use IO::Socket::INET;
 use Python::Serialise::Pickle qw();
 
-my($carbon_server) = 'docker2';
-my($carbon_port) = 2004;
+my $sock;
+my $carbon_server;
+my $carbon_port;
+my $line;
 
-my($data) = [
-             ["path.mytest", [1332444075,27893687]],
-             ["path.mytest", [1332444076,938.435]],
-            ];
+$| = 0;
 
-my($message) = pack("N/a*", pickle_dumps($data));
+$carbon_server = $ENV{'GRAFANA_PORT_2004_TCP_ADDR'};
+$carbon_port   = $ENV{'GRAFANA_PORT_2004_TCP_PORT'};
 
-my($sock) = IO::Socket::INET->new (
-                PeerAddr => $carbon_server,
-                PeerPort => $carbon_port,
-                Proto => 'tcp'
-            );
-            die "Unable to connect: $!\n" unless ($sock->connected);
+$sock = IO::Socket::INET->new (
+          PeerAddr => $carbon_server,
+          PeerPort => $carbon_port,
+          Proto => 'tcp'
+        );
+die "Unable to connect: $!\n" unless ($sock->connected);
 
-$sock->send($message);
+while($line = <>) {
+  
+  my $data = [["event", [time(), 1]]];
+
+#my($data) = [
+#             ["path.mytest", [1332444075,27893687]],
+#             ["path.mytest", [1332444076,938.435]],
+#            ];
+
+  my $message = pack("N/a*", pickle_dumps($data));
+  $sock->send($message);
+}
+
 $sock->shutdown(2);
 
 # Work around P::S::Pickle 0.01's extremely limiting interface.
