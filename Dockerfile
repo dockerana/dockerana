857c46ec54ae973cd99ef44c454e354fb187adf8
George Lewis <schvin@schvin.net>
2014-06-10 00:54:57 +0000

    clean up dockerana startup

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,7 @@ RUN apt-get install -y sysstat make
 RUN perl -MCPAN -e 'install Python::Serialise::Pickle; install Net::Statsd'
 
 ADD scripts/ingest.pl /usr/local/bin/
-ADD scripts/periodic-loop.pl /usr/local/bin/
+ADD scripts/runner.pl /usr/local/bin/
 ADD scripts/periodic-ingest.sh /usr/local/bin/
-#ADD https://get.docker.io/builds/Linux/x86_64/docker-latest /usr/local/bin/docker
-#RUN chmod 755 /usr/local/bin/docker
 
-CMD /usr/bin/tail -f /tmp/log/syslog | /usr/local/bin/ingest.pl
+CMD /usr/local/bin/runner.pl
--- scripts/runner.pl
+++ scripts/runner.pl
@@ -10,17 +10,16 @@ my $out;
 my $t0;
 my $t1;
 
-# first setup persistent stuff
-# FIXME this is going to need rotation/truncating
-#$cmd = "iostat -y 1 > /tmp/iostat";
-#$out = `$cmd &`;
+# setup syslog reader
+$cmd = "tail -f /tmp/log/syslog | /usr/local/bin/ingest.pl";
 
+/usr/local/bin/
 while(1) {
   ($t0,$t1) = gettimeofday();
   $d1 = 1000000 - $t1;
   print "t: $t0, going to sleep for $d1 usecs\n";
   usleep($d1);
-  $cmd = "./periodic-ingest.sh  | ./ingest.pl";
+  $cmd = "/usr/local/bin/periodic-ingest.sh  | /usr/local/bin/ingest.pl";
   $out = `$cmd`;
   print "out: $out\n";
 }
